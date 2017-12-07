f61b8e4458a1b12f545e200fbbc9544cdf59484d
abhinash khare <khare.abhinash4@gmail.com>
2017-12-07 20:09:59 +0530

    q20 added

--- q20.py
+++ q20.py
@@ -1,17 +1,10 @@
+fact = 1
 sum = 0
-l = [0]*1000
-l[0] = 1
+for i in range(1,101):
+    fact = fact*i
 
-def mult(l , i):
-    i = str(i)
-    for j in range(0,len(i))
-        
-    return a    
-
-for i in range(2,101):
-    l = mult(l , i)
-
-for i in range(0,len(l)):
-    sum = sum + l[i]
+fact = str(fact)
+for i in range(0,len(fact)):
+    sum += int(fact[i])
 
 print(sum)
